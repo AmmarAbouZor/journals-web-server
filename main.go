4d9346368e8d9adad5be4f4e9214407a2374a051
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-05-20 12:35:54 +0200

    Implement CRUD end points with test data

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"github.com/AmmarAbouZor/journals_backend/journals"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello journals")
+	router := gin.Default()
+
+	journalGroup := router.Group("/journal")
+
+	journalGroup.GET("", journals.GetJournals)
+	journalGroup.POST("", journals.PostJournal)
+	journalGroup.PUT("", journals.PutJournal)
+	journalGroup.DELETE("", journals.DeleteJournal)
+
+	//TODO: manage host and port with environment variables
+	router.Run("localhost:8080")
 }
